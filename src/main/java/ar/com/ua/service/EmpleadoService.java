5d8ac744b73dde35bb0c3478fa797f51a4a2b993
gmonzon <gmonzon@mazz.com.ar>
2024-01-18 17:52:48 -0300

    add search services by legajo

--- src/main/java/ar/com/ua/service/EmpleadoService.java
+++ src/main/java/ar/com/ua/service/EmpleadoService.java
@@ -1,9 +1,13 @@
 package ar.com.ua.service;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.com.ua.model.Empleado;
 
 public interface EmpleadoService extends JpaRepository<Empleado, Long> {
 	boolean existsByNumeroLegajo(Long numeroLegajo);
+
+	List<Empleado> findByNumeroLegajo(Long numeroLegajo);
 }
--- src/main/java/ar/com/ua/service/HistorialPuestosService.java
+++ src/main/java/ar/com/ua/service/HistorialPuestosService.java
@@ -1,9 +1,12 @@
 package ar.com.ua.service;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.com.ua.model.HistorialPuestos;
 
 public interface HistorialPuestosService extends JpaRepository<HistorialPuestos, Long> {
+    List<HistorialPuestos> findByNumeroLegajo(String numeroLegajo);
 
 }
