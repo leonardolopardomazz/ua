c7822b92e680920d20e171513b71703a8b806598
llopardo@mazz.com.ar <llopardo@mazz.com.ar>
2024-02-23 21:35:41 -0300

    reportes

--- src/main/java/ar/com/ua/repository/report/CentroDeCostoRepository.java
+++ src/main/java/ar/com/ua/repository/report/CentroDeCostoRepository.java
@@ -25,7 +25,7 @@ public interface CentroDeCostoRepository extends JpaRepository<Empleado, Long> {
 			+ "FROM empleados emp JOIN puesto p ON emp.cod_puesto = p.id "
 			+ "WHERE (emp.cod_centro_de_costo = :idCentroDeCosto OR :idCentroDeCosto IS NULL) "
 			+ "AND (emp.cod_direccion = :idDireccion OR :idDireccion IS NULL) "
-			+ "AND (emp.cod_estado_empleado IN :estado) "
+			+ "AND (emp.cod_estado_empleado IN ( :estado )) "
 			, nativeQuery = true)
 	List<CentroDeCostoProjection> reporte(@Param("idCentroDeCosto") String idCentroDeCosto, @Param("estado") List<String> estado,
 			@Param("idDireccion") String idDireccion);
--- src/main/java/ar/com/ua/repository/report/LicenciasRepository.java
+++ src/main/java/ar/com/ua/repository/report/LicenciasRepository.java
@@ -33,7 +33,7 @@ public interface LicenciasRepository extends JpaRepository<Empleado, Long> {
 			+ "WHERE (emp.cod_estado_empleado IN :estadoEmpleado) "
 			+ "AND (emp.cod_pais = :idPais OR :idPais IS NULL) "
 			+ "AND (p.cod_direccion = :idDireccion OR :idDireccion IS NULL) "
-			+ "AND (hdl.activo IN :estadoLicencia) ", 
+			+ "AND (hdl.activo IN ( :estadoLicencia )) ", 
 			nativeQuery = true)
 	List<LicenciasProjection> reporte(@Param("estadoEmpleado") List<String> estadoEmpleado, @Param("idPais") String pais,
 			@Param("idDireccion") String direccion, @Param("estadoLicencia") List<String> estadoLicencia);
